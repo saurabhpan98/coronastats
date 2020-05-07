8bb0da6de9dba05020963d85711d7b9d9db7a85f
saurabhpan98 <srbhpanchal98@gmail.com>
2020-05-07 17:34:53 +0530

    Updates

---
+++ precache-manifest.5babe14296a0929e66a1addcd159086e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eced251b8f99bf248a22fc496573e48",
+    "url": "/saurabhpan98/coronastats.git/index.html"
+  },
+  {
+    "revision": "ad5566926c435d905fd0",
+    "url": "/saurabhpan98/coronastats.git/static/css/main.50b664b2.chunk.css"
+  },
+  {
+    "revision": "8df4e67e3434d87a1849",
+    "url": "/saurabhpan98/coronastats.git/static/js/2.c267beed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/saurabhpan98/coronastats.git/static/js/2.c267beed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad5566926c435d905fd0",
+    "url": "/saurabhpan98/coronastats.git/static/js/main.021f3cc1.chunk.js"
+  },
+  {
+    "revision": "ceaee47a3568fbacd6e0",
+    "url": "/saurabhpan98/coronastats.git/static/js/runtime-main.d9075ee6.js"
+  },
+  {
+    "revision": "0f2fb1f646fd12e9a6c868ccb00ad9f1",
+    "url": "/saurabhpan98/coronastats.git/static/media/calling.0f2fb1f6.svg"
+  },
+  {
+    "revision": "095a1cc7dbf7827d786709f0e16545db",
+    "url": "/saurabhpan98/coronastats.git/static/media/cover cough.095a1cc7.svg"
+  },
+  {
+    "revision": "3d36874b10ce6893beb601298576272f",
+    "url": "/saurabhpan98/coronastats.git/static/media/doctor.3d36874b.svg"
+  },
+  {
+    "revision": "d05a3b460a0239826f9704573b113ea7",
+    "url": "/saurabhpan98/coronastats.git/static/media/social distancing.d05a3b46.svg"
+  },
+  {
+    "revision": "58fb1363d9390e572d23fcfd5e698afb",
+    "url": "/saurabhpan98/coronastats.git/static/media/stay home.58fb1363.svg"
+  },
+  {
+    "revision": "c3c4c6a0eb933c297248e27ebc0b6513",
+    "url": "/saurabhpan98/coronastats.git/static/media/wash hands.c3c4c6a0.svg"
+  }
+]);
